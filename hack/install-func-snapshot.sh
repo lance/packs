084d3fbd65f68cd665993bfb7d9dbd7fb2fa936b
Lance Ball <lball@redhat.com>
2021-08-23 20:56:07 -0400

    fixup

--- hack/install-func-snapshot.sh
+++ hack/install-func-snapshot.sh
@@ -10,6 +10,7 @@ mkdir -p "$WD/bin/"
 # install latest from main branch
 TMP_DIR="$(mktemp -d)"
 cd "$TMP_DIR"
+echo "TODO: CHANGE THIS TO knative-sandbox WHEN https://github.com/knative-sandbox/kn-plugin-func/pull/485/files lands"
 git clone https://github.com/lance/func
 cd func
 git checkout lance/builtin-language-packs
